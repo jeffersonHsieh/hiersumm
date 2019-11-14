ed63eb9eb637e29c424fa2b0ddd7f3a5510f5493
jeffersonHsieh <jefferson.hsieh@yale.edu>
2019-11-13 21:36:42 -0500

    debug

--- src/abstractive/trainer_ext.py
+++ src/abstractive/trainer_ext.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 from tensorboardX import SummaryWriter
 
-import distributed
+from others import distributed
 from models.reporter_ext import ReportMgr, Statistics
 from others.logging import logger
 from others.utils import test_rouge, rouge_results_to_str
