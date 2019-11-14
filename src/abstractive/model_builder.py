c268f4a6dec7373ae05db3ab6d43e0216735e5eb
jeffersonHsieh <jefferson.hsieh@yale.edu>
2019-11-13 22:35:17 -0500

    initialize linear layer param

--- src/abstractive/model_builder.py
+++ src/abstractive/model_builder.py
@@ -182,6 +182,8 @@ class ExtSummarizer(nn.Module):
             print(model_dict)
             checkpoint['model'] = {k: v for k, v in checkpoint['model'].items() if k in model_dict}
             model_dict.update(checkpoint)
+            xavier_uniform_(checkpoint['model']['wo.weight'])
+            xavier_uniform_(checkpoint['model']['wo.bias'])
             self.load_state_dict(checkpoint['model'], strict=True)
         else:
             for p in self.parameters():
