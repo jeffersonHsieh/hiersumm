a1e1d95ef2ec129937082bb841e079e362e19d26
jeffersonHsieh <jefferson.hsieh@yale.edu>
2019-11-18 17:23:05 -0500

    block trigram

--- src/train.py
+++ src/train.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
                         help="Use an extractive model")
     parser.add_argument("-ext_update_encoder", type=str2bool,nargs='?', const=True,default=False,
                         help="Update parameters in encoder as well")
-    parser.add_argument("-block_tri", type=str2bool,nargs='?', default=False,const=True,
+    parser.add_argument("-block_trigram", type=str2bool,nargs='?', default=False,const=True,
                         help="Use block trigram in extractive summary generation")
     #memory layers
     parser.add_argument("--use_memory", type=bool_flag, default=False,
